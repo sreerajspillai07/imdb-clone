fa1f0e6839e6a603082724fff932e4162d08a657
unknown <sreerajspillai07.com>
2022-11-10 01:15:41 +0530

    create header component

--- Components/Header.js
+++ Components/Header.js
@@ -1,7 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
+import HeaderIcon from './HeaderIcon'
+import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/react/24/solid'
 
-export default function Header() {
+
+export default function Navbar() {
   return (
-    <div>Header</div>
+      <div>
+          <div className="">
+              <HeaderIcon Icon={HomeIcon} title="HOME" />
+              <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+              <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+              <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+          </div>
+          <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+              width={100}
+              height={100}
+              
+          />
+    </div>
   )
 }
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Header() {
   return (
-    <div>Navbar</div>
+    <div>Header</div>
   )
 }
