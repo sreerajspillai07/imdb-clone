cc3ddea9f2532f3e44135a75e71cd7ae66b128de
unknown <sreerajspillai07.com>
2022-11-10 02:28:33 +0530

    styled header segment

--- Components/Header.js
+++ Components/Header.js
@@ -6,17 +6,18 @@ import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/re
 
 export default function Navbar() {
   return (
-      <div>
-          <div className="">
+      <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none lg:mx-6 sm:flex-row justify-between">
+          <div className="flex mb-4">
               <HeaderIcon Icon={HomeIcon} title="HOME" />
               <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
               <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
               <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
           </div>
-          <Image
+          <Image 
               src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
               width={100}
               height={100}
+              className="cursor-pointer active:brightness-200"
               
           />
     </div>
--- Components/HeaderIcon.js
+++ Components/HeaderIcon.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function HeaderIcon({Icon,title}) {
   return (
-      <div>
-          <Icon className="h-5"/>
-          <p>{ title}</p>
+      <div className="mx-4 flex flex-col cursor-pointer hover:text-yellow-400 active:text-yellow-400">
+          <Icon className="h-8"/>
+          <p className="my-2">{ title}</p>
     </div>
   )
 }
